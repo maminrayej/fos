99bf31eac255de7159577a8b39f5be9d41ea4d2e
m.amin.rayej <m.amin.rayej@gmail.com>
2020-08-25 17:48:16 +0430

    init fos, say hello world

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![no_main]
+
+// extern crate rlibc;
+
+use core::panic::PanicInfo;
+
+// Function is called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0x0f;
+        }
+    }
+
+    loop {}
+}
