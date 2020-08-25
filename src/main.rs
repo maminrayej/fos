3a10f14f4143cccd6f607fbf79146487e7b15337
m.amin.rayej <m.amin.rayej@gmail.com>
2020-08-26 03:53:58 +0430

    implement simple driver for VGA text buffer

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,22 @@
 #![no_std]
 #![no_main]
 
-// extern crate rlibc;
+mod vga_buffer;
+
+extern crate rlibc;
 
 use core::panic::PanicInfo;
 
 // Function is called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
 
-
-static HELLO: &[u8] = b"Welcome to fos!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0x0f;
-        }
-    }
+    println!("Welcome to fos!");
 
     loop {}
 }
