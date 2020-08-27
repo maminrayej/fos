9527f209500fdf873f53aa35bb05a133cb7c5ed2
m.amin.rayej <m.amin.rayej@gmail.com>
2020-08-28 01:32:35 +0430

    add support for integration tests for kernel

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -9,23 +9,17 @@ use fos::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Welcome to fos!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     fos::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
